3f0b0424f5d6a099bfc1586c9dd07295df24b597
AnastasiiaAleks <chiffka@gmail.com>
2024-02-18 02:41:02 +0300

    New Project - Game - Start2

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,10 @@ public class Main {
         Hero hero4 = new Mage("Джайна", 10, "Посох", 13, 18);
         Hero hero5 = new Spearman("Копейщик", 10, "Копье", 12, 10);
         Hero hero6 = new Crossbowman("Арбалетчик", 10, "Арбалет", 10, 15);
-        Hero hero7 = new Monk("Монах", 10, "Посох", 15, 20, 17);
+        Hero hero7 = new Monk("Монах", 15, "Посох", 15, 20, 17);
         System.out.println(hero1 + "\n" + hero2 + "\n" + hero3 + "\n"
         + hero4 + "\n" + hero5 + "\n" + hero6 + "\n" + hero7);
+
     }
+
 }
